dd1c8efe876f539c3d8de0f9fcac92c284c05706
Tyler Lentz <tlentz@ucsd.edu>
2023-12-27 23:01:37 -0800

    shift to card layout for known users

--- src/lib/KnownUsersModal.svelte
+++ src/lib/KnownUsersModal.svelte
@@ -5,19 +5,13 @@
 	import type { Profile } from '$lib/bindings/Profile';
 	import { invoke } from '@tauri-apps/api';
 	import type { KnownUsers } from '$lib/bindings/KnownUsers';
-    import arrowLeft from "$lib/icons/arrow_left.svg"
-    import arrowRight from "$lib/icons/arrow_right.svg"
-	import Canvas from './Canvas.svelte';
-	import { PROFILE_PIC_SIZE } from './contants';
+	import ProfileCard from '$lib/ProfileCard.svelte';
+	import { profile } from '$lib/stores';
 
     export let isOpen: boolean;
     export let startClose: Writable<boolean>;
 
     let profiles: Profile[] = [];
-    let index: number = 0;
-
-    let profile: Profile | undefined;
-    $: profile = profiles.at(index);
 
     onMount(() => {
         invoke("cmd_get_known_users")
@@ -26,24 +20,6 @@
             })
     });
 
-    function wrapIndex() {
-        if (index < 0) {
-            index = profiles.length - 1;
-        }
-        if (index >= profiles.length) {
-            index = 0;
-        }
-    }
-
-    function moveLeft() {
-        index--;
-        wrapIndex();
-    }
-
-    function moveRight() {
-        index++;
-        wrapIndex();
-    }
 </script>
 
 <GenericModal
@@ -53,33 +29,16 @@
     >
     <div id="modal-container">
         <div id="profile-container">
-            {#if profile !== undefined}
-                <h2>{profile.name}</h2>
-                <Canvas 
-                    width={PROFILE_PIC_SIZE}
-                    height={PROFILE_PIC_SIZE}
-                    editable={false}
-                    data={profile.pic}
-                    />
-            {/if}
-        </div>
-        <div id="controls-container">
-            <button class="icon-btn" on:click={moveLeft} >
-                <img src={arrowLeft} alt={"Left"} />
-            </button>
-            <button class="icon-btn" on:click={moveRight} >
-                <img src={arrowRight} alt={"Right"} />
-            </button>
+            {#each profiles as curr_profile}
+                {#if curr_profile.uid != $profile?.uid}
+                    <ProfileCard profile={curr_profile} />
+                {/if}
+            {/each}
         </div>
     </div>
 </GenericModal>
 
 <style>
-    h2 {
-        text-align: center;
-        font-size: 24px;
-    }
-
     #modal-container {
         display: flex;
         flex-direction: column;
@@ -89,11 +48,12 @@
     }
 
     #profile-container {
-        margin-bottom: auto;
-    }
-
-    #controls-container {
+        height: 100%;
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
     }
+
 </style>
